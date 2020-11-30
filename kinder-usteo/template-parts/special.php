0a7abea8a750174d03d8ed274621c7f2b9ee8a1f
vikkerus <49053629+vikkerus@users.noreply.github.com>
2020-11-30 09:13:06 +0600

    Update special.php

--- kinder-usteo/template-parts/special.php
+++ kinder-usteo/template-parts/special.php
@@ -5,38 +5,44 @@
 	<div class="aside-widget-body">
 		<ul class="list-unstyled standart-oo-menu">
 			<li>
-			<a href="<?php echo site_url('/sveden/common');?>" title="Основные сведения">Основные сведения</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/struct');?>" title="Структура и органы управления образовательной организацией">Структура и органы управления образовательной организацией</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/document');?>" title="Документы">Документы</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/education');?>" title="Образование">Образование</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/edustandarts');?>" title="Образовательные стандарты">Образовательные стандарты</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/employees');?>" title="Руководство. Педагогический (научно-педагогический) состав">Руководство. Педагогический (научно-педагогический) состав</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/objects');?>" title="Материально-техническое обеспечение и оснащенность образовательного процесса">Материально-техническое обеспечение и оснащенность образовательного процесса</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/grants');?>" title="Стипендии и иные виды материальной поддержки">Стипендии и иные виды материальной поддержки</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/paid_edu');?>" title="Платные образовательные услуги">Платные образовательные услуги</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/budget');?>" title="Финансово-хозяйственная деятельность">Финансово-хозяйственная деятельность</a>
-		</li>
-		<li>
-			<a href="<?php echo site_url('/sveden/vacant');?>" title="Вакантные места для приема (перевода)">Вакантные места для приема (перевода)</a>
-		</li>
+                <a href="<?php echo site_url('/sveden/common');?>" title="Основные сведения">Основные сведения</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/struct');?>" title="Структура и органы управления образовательной организацией">Структура и органы управления образовательной организацией</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/document');?>" title="Документы">Документы</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/education');?>" title="Образование">Образование</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/edustandarts');?>" title="Образовательные стандарты">Образовательные стандарты</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/employees');?>" title="Руководство. Педагогический (научно-педагогический) состав">Руководство. Педагогический (научно-педагогический) состав</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/objects');?>" title="Материально-техническое обеспечение и оснащенность образовательного процесса">Материально-техническое обеспечение и оснащенность образовательного процесса</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/grants');?>" title="Стипендии и иные виды материальной поддержки">Стипендии и иные виды материальной поддержки</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/paid_edu');?>" title="Платные образовательные услуги">Платные образовательные услуги</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/budget');?>" title="Финансово-хозяйственная деятельность">Финансово-хозяйственная деятельность</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/vacant');?>" title="Вакантные места для приема (перевода)">Вакантные места для приема (перевода)</a>
+            </li> 
+            <li>
+                <a href="<?php echo site_url('/sveden/ovz');?>" title="Доступная среда">Доступная среда</a>
+            </li>
+            <li>
+                <a href="<?php echo site_url('/sveden/inter');?>" title="Международное сотрудничество">Международное сотрудничество</a>
+            </li>
 		</ul>
 	</div>
 </div>
