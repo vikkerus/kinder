526f6a287d4a1d0f3b38855fc405aa3188c80868
vikkerus <49053629+vikkerus@users.noreply.github.com>
2021-09-06 16:12:16 +0600

    Update functions.php

--- kinder-usteo/functions.php
+++ kinder-usteo/functions.php
@@ -509,8 +509,6 @@ function kinder_scripts()
 	
 	wp_enqueue_script( 'kinder_app', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array(), null, true);
 	
-	wp_enqueue_script( 'kinder_app', get_stylesheet_directory_uri() . '/assets/js/app.js', array(), null, true);
-	
 }
 
 add_action( 'wp_enqueue_scripts', 'kinder_scripts' );
